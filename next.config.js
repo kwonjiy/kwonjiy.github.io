6f849e63390ebaa43095db8cb9aeae18161fb9cb
kwon jiyeon <kwonjiiy@gmail.com>
2025-01-01 18:45:45 +0900

    Remove API routes for static export

--- next.config.js
+++ next.config.js
@@ -5,9 +5,6 @@ const nextConfig = {
     domains: ['fglapqpkzzqcrqwbjpft.supabase.co'],
     unoptimized: true,
   },
-  experimental: {
-    serverActions: true
-  },
   typescript: {
     // !! WARN !!
     // 프로덕션에서는 이 옵션을 false로 설정하세요!
